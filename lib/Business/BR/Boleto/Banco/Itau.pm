cc29cfb98f0f972079d3eb6ba6e024d841a167b9
Blabos de Blebe <blabos@gmail.com>
2014-11-25 13:20:02 -0200

    WIP

--- lib/Business/BR/Boleto/Banco/Itau.pm
+++ lib/Business/BR/Boleto/Banco/Itau.pm
@@ -5,22 +5,23 @@ with 'Business::BR::Boleto::Role::Banco';
 
 use Business::BR::Boleto::Utils qw{ mod10 mod11 };
 
-sub nome       { 'Itau' }
-sub codigo     { '341' }
-sub pre_render { }
+sub nome   { 'Itau' }
+sub codigo { '341' }
 
 sub campo_livre {
     my ( $self, $cedente, $pagamento ) = @_;
-
-    my $campo_livre = '';
 
     my $nosso_numero = sprintf '%08d', $pagamento->nosso_numero;
     my $carteira     = sprintf '%03d', $cedente->carteira;
     my $agencia      = sprintf '%04d', $cedente->agencia->{numero};
     my $conta        = sprintf '%05d', $cedente->conta->{numero};
+    my $conta_dv     = $cedente->conta->{dv};
 
     my $dac_nn = mod10( $agencia . $conta . $carteira . $nosso_numero );
     my $dac_ac = mod11( $agencia . $conta );
+
+    $self->codigo_cedente( $agencia . '/' . $conta . '-' . $conta_dv );
+    $self->nosso_numero( $carteira . '/' . $nosso_numero . '-' . $dac_nn );
 
     return
         $carteira
